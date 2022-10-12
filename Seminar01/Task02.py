58ebf8921140eaed8bfdd1911bae0ddf24550d10
Lekxpa <LekxpaP@gmail.com>
2022-10-12 23:30:23 +0300

    correct decision in Task02

--- Seminar01/Task02.py
+++ Seminar01/Task02.py
@@ -1,2 +1,10 @@
 # Напишите программу для. проверки истинности утверждения 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат. матрица
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if(not(x or y or z) == (not x and not y and not z)):
+                print(f'{x} {y} {z} - true')
+            else:
+                print(f'{x} {y} {z} - false')
