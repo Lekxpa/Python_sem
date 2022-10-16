aa579f4edee707d67d62baaba6becce2de89e7e6
Lekxpa <LekxpaP@gmail.com>
2022-10-17 00:44:59 +0300

    correct decision Task03 in Seminar02

--- Seminar02/Task03.py
+++ Seminar02/Task03.py
@@ -12,27 +12,5 @@ a = 1
 for i in range(n):
     a = (1 + 1 / a) ** a
     sumn = a + sumn
-    print(round ((a),8), end = ' ')
-print('\n' 'Сумма чисел равна', round((sumn), 2))
-
-# sequence = [((1 + 1 / i) ** i) for i in range(1, num+1)]
-
-# def numbers(n):
-#     summ = 0
-#     for i in range(n):
-#         a = int(input(f'Введи число {i + 1} '))
-#         a = (1+1/a)**a
-#         summ = a + summ
-#         i += 1
-#     return summ
-
-# print('Сумма чисел равна',round((numbers(n)), 2))
-
-# import decimal
-# import random
-# import re
-
-# def test3():
-#     num = int(entertext('введите натуральное число ', r'\d{0,}[1-9]{1}\d{0,}'))
-#     sequence = [((1 + 1 / i) ** i) for i in range(1, num+1)]
-#     print(sequence, sum(sequence))
+    print(round ((a),4), end = ' ')
+print('\n' 'Сумма чисел равна', round((sumn), 4))
