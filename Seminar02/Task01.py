696173c99eaf98cc5198b2e6997df0038ac8bbc4
Lekxpa <LekxpaP@gmail.com>
2022-10-17 00:38:09 +0300

    add decision in Task01 in Seminar02

--- Seminar02/Task01.py
+++ Seminar02/Task01.py
@@ -4,26 +4,11 @@
 #- 6782 -> 23
 #- 0,56 -> 11
 
-def s(n):
-    if n < 0:
-        n *= (-1)
-       
-    s = str(n)
-    if '.' in s:
-        return abs(s.find('.') - len(s)) - 1
-    else:
-        return 0
-n = n * 10 ** a
-couunt = 0
-    while n > 0:
-        count += n % 10
-        n //= 10
-    return count
-n = float(input('Введите число: '))
-print(s(n))
-
-def num_after_point(x):
-    s = str(x)
-    if not '.' in s:
-        return 0
-    return len(s) - s.index('.') - 1
+n = input('Ведите число: ')
+res = 0
+# if n < 0:
+#     n *= (-1)
+for i in n:
+    if i != ',' and i != '.':
+        res += int(i)
+print(res)
