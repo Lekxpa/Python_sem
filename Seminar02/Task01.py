9ca75fc287302d2ee230674da81f60315fa1bb02
Lekxpa <LekxpaP@gmail.com>
2022-10-17 19:32:43 +0300

    correct file Task03 in Seminar02

--- Seminar02/Task01.py
+++ Seminar02/Task01.py
@@ -4,11 +4,26 @@
 #- 6782 -> 23
 #- 0,56 -> 11
 
+# n = input('Ведите число: ')
+# print(lenght)
+# res = 0
+# # if n < 0:
+# #     n *= (-1)
+# for i in n:
+#     if i != ',' and i != '.':
+#         res += int(i)
+# print(res)
+
+
 n = input('Ведите число: ')
 res = 0
-# if n < 0:
-#     n *= (-1)
-for i in n:
-    if i != ',' and i != '.':
-        res += int(i)
+if n == ',' and n == '.':
+    lenght = len(str(n))
+    print(lenght)
+    n = n * (10 ** lenght)
+elif n < 0:
+    n *= (-1)
+while n != 0:
+    res = res + (int(n) % 10)
+    n = int(n) // 10
 print(res)
--- Seminar02/Task03.py
+++ Seminar02/Task03.py
@@ -6,11 +6,13 @@
     # Для n=4 {1: 2, 2: 2.25, 3: 2.37, 4: 2.44}
     # Сумма 9.06
 
-n = int(input('Введите число n: '))
-sumn = 0
-a = 1
-for i in range(n):
-    a = (1 + 1 / a) ** a
-    sumn = a + sumn
-    print(round ((a),4), end = ' ')
-print('\n' 'Сумма чисел равна', round((sumn), 4))
+def SummOfElements():
+    n = int(input('Введите число n: '))
+    sumN = 0
+    numbers = {}
+    for i in range (1, n + 1):
+        numbers[i] = round((1 + 1 / i) ** i, 2)
+        sumN = numbers[i] + sumN
+    print(numbers)
+    print('\n' 'Сумма чисел равна: ', round((sumN), 2))
+SummOfElements()
