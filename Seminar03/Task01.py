a91365ed12163ecf9ea9b967340a53d7e4269f4e
Lekxpa <LekxpaP@gmail.com>
2022-10-18 16:00:44 +0300

    add decision in Task01 in Seminar03

--- Seminar03/Task01.py
+++ Seminar03/Task01.py
@@ -2,3 +2,19 @@
 # которая найдёт сумму элементов списка, стоящих на нечётной позиции.
 # Пример:
 # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
+
+import random
+
+N = int(input('Введите количество элементов в списке: '))
+numbers = []
+for i in range(N):
+    numbers.append(random.randint(0,N))
+print(f'Список: {numbers}')
+
+res = 0
+index = 1
+while index < len(numbers):
+    res = res + numbers[index]
+    print(numbers[index], end = '  ')
+    index +=2
+print('\n' f'Сумма элементов нечетных позиций = {res}')
