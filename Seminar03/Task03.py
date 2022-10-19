72a371b42688f4209d1dae22fd853f3d3d99743d
Lekxpa <LekxpaP@gmail.com>
2022-10-19 15:53:12 +0300

    correct decision in Task03 in Seminar03

--- Seminar03/Task03.py
+++ Seminar03/Task03.py
@@ -3,3 +3,14 @@
 # дробной части элементов.
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
+
+
+my_list = [1.1, 1.2, 3.1, 5, 10.01]
+min = 1
+max = 0
+for i in my_list:
+    if 0 < (i - int(i)) <= min:
+        min = i - int(i)
+    if (i - int(i)) >= max:
+        max = i - int(i)  
+print(round((max-min), 2))
--- Seminar03/Task05.py
+++ Seminar03/Task05.py
@@ -5,3 +5,42 @@
 # - для k = 8 список будет выглядеть так: 
 # [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21] 
 # [Негафибоначчи]
+
+
+
+
+
+# def FibanachiNEgativeAndPositive(argFib):
+#     """
+#     Positive part
+#     """
+#     fibPosit = []
+#     if argFib < 1:
+#         print("Empty list")
+#         return fibPosit
+#     else:
+#         fibPosit.append(1)
+#     if argFib > 1:
+#         fibPosit.append(1)
+#     else:
+#         return [1, 0, 1]  
+#     if argFib > 2:
+#         for i in range(2, argFib):
+#             fibPosit.append(fibPosit[i - 1] + fibPosit[i - 2])
+#         """
+#         Positive part
+#         """
+#         fibNegat = fibPosit.copy()
+#         for i in range(argFib):
+#             fibNegat[i] = fibNegat[i] * (-1) ** i
+#         fibNegat.reverse()
+#         """
+#         Concatination
+#         """
+#         fibFull = fibNegat + [0] + fibPosit
+#         return fibFull
+#     else:
+#         return [-1, 1, 0, 1, 1]
+
+# n = int(input('Enter the argument for Fibanachi \n'))
+# print (FibanachiNEgativeAndPositive(n))
