bb0357791860f3861cc40f835ce375ee69b54957
Lekxpa <LekxpaP@gmail.com>
2022-11-29 14:54:59 +0300

    little changes

---
+++ .folder/bot.py
@@ -0,0 +1,19 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello, {update.effective_user.first_name}')
+
+async def bye(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f" See you, {update.effective_user.first_name}")
+
+
+app = ApplicationBuilder().token("5803182677:AAE7-5Q2HEEQTEibl1KayeA_IXMrEQsokd4").build()
+
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("bye", bye))
+
+app.run_polling()
+
+# t.me/Test_gb_lekbot
--- .folder/piptest1.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-from imojify import imojify
-
-Image.open(imojify.get_img_path('😂'))
